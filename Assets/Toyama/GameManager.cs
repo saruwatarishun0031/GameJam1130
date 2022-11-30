90777cd84912788bbc9026f62757c54136f9356e
sinzitoyama <yamat422@gmail.com>
2022-11-30 13:38:18 +0900

    GameManager

--- Assets/Toyama/GameManager.cs
+++ Assets/Toyama/GameManager.cs
@@ -2,17 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class GameManager :MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public bool _start = false;//true時にゲームスタート
+    public float _time = 0f;
+    [SerializeField,Header("ゲームオーバー時間")] float _gameOverTime = 5f;
+    [SerializeField,Header("ゲームオーバー時のパネル")] GameObject _panel;
+
+    private void Start()
     {
-        
+        GameStart();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        if(_start)
+        {
+            _time += Time.deltaTime;
+            if (_time > _gameOverTime)
+            {
+                GameOver();
+            }
+        }
+        else
+        {
+            return;
+        }
+        
+    }
+    public void GameStart()
+    {
+        StartCoroutine(Hoge());
+        IEnumerator Hoge()
+        {
+            yield return new WaitUntil(() => Input.GetButtonDown("Jump"));
+            Debug.Log("Start");
+            _start = true;
+        }
+    }
+    public void GameOver()
+    {
+        _start = false;
+        _time = 0f;
+        Debug.Log("iikanzinopanel");
+        _panel.SetActive(true);
         
     }
 }
+
