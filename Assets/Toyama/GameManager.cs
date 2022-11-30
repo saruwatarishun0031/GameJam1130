3ca2fe90e5c6d112ef2dc932453883b1b29b0724
sinzitoyama <yamat422@gmail.com>
2022-11-30 14:22:19 +0900

    GameManeger

--- Assets/Toyama/GameManager.cs
+++ Assets/Toyama/GameManager.cs
@@ -1,26 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager :MonoBehaviour
 {
     public bool _start = false;//true時にゲームスタート
     public float _time = 0f;
+    private bool _stop = false;
+    public int _largeCount = 0;
+    public int _midiumCount = 0;
+    public int _smailCount = 0;
+    [SerializeField, Header("タイマー")] Text _timeText;
+    [SerializeField, Header("魚大")] Text _largeText;
+    [SerializeField, Header("魚中")] Text _midiumText;
+    [SerializeField, Header("魚小")] Text _smailText;
     [SerializeField,Header("ゲームオーバー時間")] float _gameOverTime = 5f;
     [SerializeField,Header("ゲームオーバー時のパネル")] GameObject _panel;
 
     private void Start()
     {
         GameStart();
+
     }
 
     private void Update()
     {
         if(_start)
         {
-            _time += Time.deltaTime;
+            _largeText.text = _largeCount.ToString();
+            _midiumText.text = _midiumCount.ToString();
+            _smailText.text = _smailCount.ToString();
+            
+            if (!_stop)
+            {
+                _time += Time.deltaTime;
+                _timeText.text = _time.ToString("F1");
+            }
             if (_time > _gameOverTime)
             {
+                _stop = true;
                 GameOver();
             }
         }
@@ -29,6 +48,25 @@ public class GameManager :MonoBehaviour
             return;
         }
         
+    }
+    public void FishCount(int i)
+    {
+        if(i == 3)
+        {
+            _largeCount++;
+        }
+        else if(i == 2)
+        {
+            _midiumCount++;
+        }
+        else if(i == 1)
+        {
+            _smailCount++;
+        }
+        else
+        {
+            return;
+        }
     }
     public void GameStart()
     {
